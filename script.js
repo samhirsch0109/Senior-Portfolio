9cb5319c5d4bff535f4077f1e6d85781ce248a76
samhirsch0109 <144719425+samhirsch0109@users.noreply.github.com>
2025-06-03 18:21:29 +0000

    ok might be done for real

--- script.js
+++ script.js
@@ -33,47 +33,6 @@ $(document).ready(function () {
 });
 
 
-
-const vueApp = Vue.createApp({
-    data() {
-        return {
-            pageTitle: 'Gallery',
-            projects: [],
-        };
-    },
-    methods: {
-        openModal(index) {
-            const modalId = '#carouselModal' + index;
-            const modalEl = document.querySelector(modalId);
-            const modal = new bootstrap.Modal(modalEl);
-            modal.show();
-        },
-        toggleDescription(index) {
-            this.projects[index].showDescription = !this.projects[index].showDescription;
-        },
-        async fetchProjects() {
-            const res = await fetch('projects.json');
-            const data = await res.json();
-
-            // Ensure each project has a description and toggle state
-            data.forEach(project => {
-                if (!Array.isArray(project.description)) {
-                    project.description = [];
-                }
-                project.showDescription = false;
-            });
-
-            this.projects = data;
-        }
-    },
-    mounted() {
-        this.fetchProjects();
-    }
-});
-
-vueApp.mount('#vue_app');
-
-
  const sections = document.querySelectorAll("section");
   const navLinks = document.querySelectorAll(".nav-link");
 
